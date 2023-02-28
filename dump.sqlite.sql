afe7266bf875371bb876b474397f2205e20f4e0c
Jonatha <104457058+JonathaVitor@users.noreply.github.com>
2023-02-28 16:06:17 -0300

    cad-log
    BD cadastro e login
    Projeto Pupyla...

---
+++ dump.sqlite.sql
@@ -0,0 +1,11 @@
+-- TABLE
+CREATE TABLE cadastro (usuario varchar(15), email varchar(40), senha varchar(20));
+CREATE TABLE login (id integer Primary KEY AUTOINCREMENT, email varchar(40), senha varchar(20));
+CREATE TABLE sqlite_sequence(name,seq);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
